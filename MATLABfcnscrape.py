139d9dc16b2a8b9022e514829607375dfd58ee07
sco1 <sco1.git@gmail.com>
2019-04-14 17:02:13 -0400

    Black pass

--- MATLABfcnscrape.py
+++ MATLABfcnscrape.py
@@ -7,18 +7,25 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 
+
 # Force UTC Timestamps
 # From the logging cookbook: https://docs.python.org/3/howto/logging-cookbook.html
 class UTCFormatter(logging.Formatter):
     converter = time.gmtime
 
-logformat = '%(asctime)s %(levelname)s:%(module)s:%(message)s'
-dateformat = '%Y-%m-%d %H:%M:%S'
-logging.basicConfig(filename='./log/scrape.log', filemode='a', level=logging.INFO, 
-                    format=logformat, datefmt=dateformat
-                    )
 
-def loadURLdict(sourceJSON='./fcnURL.JSON'):
+logformat = "%(asctime)s %(levelname)s:%(module)s:%(message)s"
+dateformat = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(
+    filename="./log/scrape.log",
+    filemode="a",
+    level=logging.INFO,
+    format=logformat,
+    datefmt=dateformat,
+)
+
+
+def loadURLdict(sourceJSON="./fcnURL.JSON"):
     """
     Load URL dictionary from input JSON file
 
@@ -29,11 +36,12 @@ def loadURLdict(sourceJSON='./fcnURL.JSON'):
     Output is a single layer dict containing the toolbox:url KV-pairs
     """
     sourceJSON = Path(sourceJSON)
-    with sourceJSON.open(mode='r') as fID:
+    with sourceJSON.open(mode="r") as fID:
         tmp = json.load(fID)
 
     squeezegen = (tmp[grouping] for grouping in tmp.keys())
     return {k: v for d in squeezegen for k, v in d.items()}
+
 
 def scrapedocpage(URL):
     """
@@ -41,108 +49,124 @@ def scrapedocpage(URL):
 
     Object methods (foo.bar) and comments (leading %) are excluded
 
-    Returns a list of function name strings, or an empty list if none are found (e.g. no permission for toolbox)
+    Returns a list of function name strings, or an empty list if none are found (e.g. no permission)
     """
     r = requests.get(URL, timeout=2)
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, "html.parser")
 
-    tags = soup.find_all(attrs={'class': 'function'})
-    
+    tags = soup.find_all(attrs={"class": "function"})
+
     # Iterate through tags & apply filters before appending to function list
     fcns = []
     for tag in tags:
         line = tag.string
         # Blacklist filters
-        if re.findall(r'[%:]', line):
+        if re.findall(r"[%:]", line):
             # Ignore lines with '%' or ':'
             continue
-        if re.match(r'^ocv', line):
+        if re.match(r"^ocv", line):
             # Ignore OpenCV C++ commands
             continue
-        elif 'ColorSpec' in line or 'LineSpec' in line:
+        elif "ColorSpec" in line or "LineSpec" in line:
             # Ignore ColorSpec and LineSpec
             # TODO: Add JSON function blacklist
             continue
 
         # Strip out anything encapsulated by parentheses or brackets
-        line = re.sub(r'[[({][A-Za-z0-9,.]+[])}]', '', line).strip()
+        line = re.sub(r"[[({][A-Za-z0-9,.]+[])}]", "", line).strip()
         # "Modification" filters
-        if ',' in line:
+        if "," in line:
             # Split up functions on lines with commas
-            [fcns.append(thing.strip()) for thing in line.split(',')]
-        elif '.' in line:
+            [fcns.append(thing.strip()) for thing in line.split(",")]
+        elif "." in line:
             # Skip regex filter for object methods
             fcns.append(line)
         else:
             # Otherwise apply a simple regex filter for the first word on a line
-            tmp = re.findall('^\w+', line)
+            tmp = re.findall(r"^\w+", line)
             if tmp:
                 fcns.append(tmp[0])
 
     return fcns
 
-def writeToolboxJSON(fcnlist, toolboxname, JSONpath='./JSONout'):
+
+def writeToolboxJSON(fcnlist, toolboxname, JSONpath="./JSONout"):
     """
     Write input toolbox function list to dest/toolboxname.JSON
     """
     JSONpath = Path(JSONpath)
-    JSONpath.mkdir(parents=True, exist_ok=True)  # Create destination folder if it doesn't already exist
-    
-    filepath = JSONpath / f'{toolboxname}.JSON'
-    with filepath.open(mode='w') as fID:
+    JSONpath.mkdir(
+        parents=True, exist_ok=True
+    )  # Create destination folder if it doesn't already exist
+
+    filepath = JSONpath / f"{toolboxname}.JSON"
+    with filepath.open(mode="w") as fID:
         json.dump(fcnlist, fID, indent="\t")
 
-def concatenatefcns(JSONpath='./JSONout', fname='_combined'):
+
+def concatenatefcns(JSONpath="./JSONout", fname="_combined"):
     """
     Generate concatenated function set from directory of JSON files and write to 'fname.JSON'
 
-    Assumes JSON file is a list of function name strings 
+    Assumes JSON file is a list of function name strings
     """
     JSONpath = Path(JSONpath)
-    outfilepath = JSONpath / f'{fname}.JSON'
+    outfilepath = JSONpath / f"{fname}.JSON"
 
     fcnset = set()
-    for fcnJSON in JSONpath.glob('*.JSON'):
-        with fcnJSON.open(mode='r') as fID:
+    for fcnJSON in JSONpath.glob("*.JSON"):
+        with fcnJSON.open(mode="r") as fID:
             fcnset.update(json.load(fID))
-    
+
     logging.info(f"Concatenated {len(fcnset)} unique functions")
-    with outfilepath.open(mode='w') as fID:
+    with outfilepath.open(mode="w") as fID:
         json.dump(sorted(fcnset, key=str.lower), fID, indent="\t")
 
-def scrapetoolboxes(URL="https://www.mathworks.com/help/index.html", JSONpath = '.', fname='fcnURL'):
+
+def scrapetoolboxes(
+    URL="https://www.mathworks.com/help/index.html", JSONpath=".", fname="fcnURL"
+):
     """
     Generate a dictionary of toolboxes & link to the alphabetical function list
 
     Dictionary is dumped to JSON/fname.JSON
     """
     r = requests.get(URL, timeout=2)
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, "html.parser")
 
     # Get first header that matches 'MATLAB', this should be our 'MATLAB Family' column
     # For some reason soup.find_all('h2', string=re.compile('MATLAB')) returns an empty list
     # Generator approach from SO: https://stackoverflow.com/a/7006873/2748311
-    matlabheader = next((t for t in soup.find_all('h2') if t.find(text=re.compile('MATLAB'))), None)
+    matlabheader = next(
+        (t for t in soup.find_all("h2") if t.find(text=re.compile("MATLAB"))), None
+    )
 
     # Get to column div from the header, this is 2 levels above
     matlabcolumn = matlabheader.parent.parent
 
-    # Iterate through MATLAB's groupings (does not include base MATLAB) and pull toolboxes & links by group
-    # Build lambda to strip spaces out of the toolbox names
-    namelambda = lambda x: x.replace(" ", "")
-
+    # Iterate through MATLAB's groupings (does not include base MATLAB) and pull toolboxes & links
     groupeddict = {}
     # Add base MATLAB manually
-    groupeddict['Base Matlab'] = {'MATLAB': "https://www.mathworks.com/help/matlab/functionlist-alpha.html"}
-    for grouping in matlabcolumn.find_all('h4'):
-        groupeddict[grouping.text] = {namelambda(listitem.text): helpURLbuilder(listitem.a.get('href'))for listitem in grouping.parent.find_all('li')}
+    groupeddict["Base Matlab"] = {
+        "MATLAB": "https://www.mathworks.com/help/matlab/functionlist-alpha.html"
+    }
+    for grouping in matlabcolumn.find_all("h4"):
+        groupeddict[grouping.text] = {
+            listitem.text.replace(" ", ""): helpURLbuilder(listitem.a.get("href"))
+            for listitem in grouping.parent.find_all("li")
+        }
 
     JSONpath = Path(JSONpath)
-    outfilepath = JSONpath / f'{fname}.JSON'
-    with outfilepath.open(mode='w') as fID:
+    outfilepath = JSONpath / f"{fname}.JSON"
+    with outfilepath.open(mode="w") as fID:
         json.dump(groupeddict, fID, indent="\t")
 
-def helpURLbuilder(shortlink, prefix="https://www.mathworks.com/help/", suffix="/functionlist-alpha.html"):
+
+def helpURLbuilder(
+    shortlink,
+    prefix="https://www.mathworks.com/help/",
+    suffix="/functionlist-alpha.html",
+):
     """
     Helper to build URL for alphabetical function list from the toolbox's shortlink
 
@@ -150,11 +174,11 @@ def helpURLbuilder(shortlink, prefix="https://www.mathworks.com/help/", suffix="
 
     Returns a string
     """
-    return prefix + shortlink.split('/')[0] + suffix
+    return prefix + shortlink.split("/")[0] + suffix
 
 
 if __name__ == "__main__":
-    outpath = './JSONout/R2018a'
+    outpath = "./JSONout/R2018a"
 
     scrapetoolboxes()
     toolboxdict = loadURLdict()
@@ -164,8 +188,10 @@ if __name__ == "__main__":
         try:
             fcnlist = scrapedocpage(URL)
             if len(fcnlist) == 0:
-                # No functions found, most likely because permission for the toolbox documentation is denied
-                logging.info(f"Permission to view documentation for '{toolbox}' has been denied: {URL}")
+                # No functions found, most likely because permission for the toolbox docs is denied
+                logging.info(
+                    f"Permission to view documentation for '{toolbox}' has been denied: {URL}"
+                )
             else:
                 writeToolboxJSON(fcnlist, toolbox, outpath)
         except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
